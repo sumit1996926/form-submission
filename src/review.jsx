73449a52cd082150abeb4e591c4decdef3039012
Sumit Yadav <sumit1996926@gmail.com>
2023-10-24 04:48:43 +0530

    form-setup

---
+++ src/review.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const ReviewForm = () => {
+  return (
+    <div>
+      <h2>Leave a Review</h2>
+      <form name="review" method="POST" data-netlify="true">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div>
+          <label htmlFor="rating">Rating:</label>
+          <input
+            type="number"
+            id="rating"
+            name="rating"
+            min="1"
+            max="5"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="comment">Comment:</label>
+          <textarea id="comment" name="comment" required></textarea>
+        </div>
+        <div>
+          <button type="submit">Submit Review</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ReviewForm;
